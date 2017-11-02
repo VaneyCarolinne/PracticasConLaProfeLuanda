221a630e8d5a2b055098511b70b1b85f8d78d22c
Vanessa Cruz <vaneycarolinne.vc@gmail.com>
2017-11-02 16:03:07 -0400

    Ejercicio lab3 y lab7
    El ejercicio lab3 solo contiene el proyecto del reloj, el lab7 ambos reloj y dispositivos.

---
+++ Utilidades/DispositivoElectronico.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilidades;
+
+/**
+ *
+ * @author Vaney
+ */
+public class DispositivoElectronico {
+    public String marca, modelo;
+
+    public DispositivoElectronico(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+}
